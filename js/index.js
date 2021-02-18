d2784a417a3a6a7501410c85d28f5cbbdaef9cfb
GalbaKor <52763544+GalbaKor@users.noreply.github.com>
2021-02-18 19:05:13 +0000

    external JS articles
    Altered article JS script to work in external js file

---
+++ js/index.js
@@ -0,0 +1,48 @@
+var ideaClick = document.getElementById("ideationcreation");
+var elevateClick = document.getElementById("elevatorpitch");
+var intClick = document.getElementById("interviewdata");
+var persClick = document.getElementById("personas");
+
+function ideationClick() {
+if (ideaClick.style.display === "none") {
+    ideaClick.style.display = "inherit";
+    elevateClick.style.display = "none";
+    intClick.style.display = "none";
+    persClick.style.display = "none";
+} else {
+    ideaClick.style.display = "none";
+}
+}
+
+function elevatorClick() {
+if (elevateClick.style.display === "none") {
+    ideaClick.style.display = "none";
+    elevateClick.style.display = "inherit";
+    intClick.style.display = "none";
+    persClick.style.display = "none";
+} else {
+    elevateClick.style.display = "none";
+}
+}
+
+function interviewClick() {
+if (intClick.style.display === "none") {
+    ideaClick.style.display = "none";
+    elevateClick.style.display = "none";
+    intClick.style.display = "inherit";
+    persClick.style.display = "none";
+} else {
+    intClick.style.display = "none";
+}
+}
+
+function personaClick() {
+if (persClick.style.display === "none") {
+    ideaClick.style.display = "none";
+    elevateClick.style.display = "none";
+    intClick.style.display = "none";
+    persClick.style.display = "inherit";
+} else {
+    persClick.style.display = "none";
+}
+}
