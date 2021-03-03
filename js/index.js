b1c5e77e331043ca493e78f1b4d539e60165deb7
GalbaKor <52763544+GalbaKor@users.noreply.github.com>
2021-03-03 23:19:06 +0000

    testing interview nav
    interview nav buttons etc

--- js/index.js
+++ js/index.js
@@ -2,6 +2,11 @@ var ideaClick = document.getElementById("ideationcreation");
 var elevateClick = document.getElementById("elevatorpitch");
 var intClick = document.getElementById("interviewdata");
 var persClick = document.getElementById("personas");
+var int1 = document.getElementById("interview1");
+var int2 = document.getElementById("interview2");
+var int3 = document.getElementById("interview3");
+var int4 = document.getElementById("interview4");
+
 
 window.onload = function(){ 
     loadFunction();
@@ -12,10 +17,15 @@ function loadFunction() {
     elevateClick.style.display = "none";
     intClick.style.display = "none";
     persClick.style.display = "none";
+
+    int1.style.display = "inherit";
+    int2.style.display = "none";
+    int3.style.display = "none";
+    int4.style.display = "none";
 }
 
 function ideationClick() {
-if (ideaClick.style.display === "none") {
+if (ideaClick.style.display = "none") {
     ideaClick.style.display = "inherit";
     elevateClick.style.display = "none";
     intClick.style.display = "none";
@@ -26,7 +36,7 @@ if (ideaClick.style.display === "none") {
 }
 
 function elevatorClick() {
-if (elevateClick.style.display === "none") {
+if (elevateClick.style.display = "none") {
     ideaClick.style.display = "none";
     elevateClick.style.display = "inherit";
     intClick.style.display = "none";
@@ -37,7 +47,7 @@ if (elevateClick.style.display === "none") {
 }
 
 function interviewClick() {
-if (intClick.style.display === "none") {
+if (intClick.style.display = "none") {
     ideaClick.style.display = "none";
     elevateClick.style.display = "none";
     intClick.style.display = "inherit";
@@ -48,12 +58,52 @@ if (intClick.style.display === "none") {
 }
 
 function personaClick() {
-if (persClick.style.display === "none") {
+if (persClick.style.display = "none") {
     ideaClick.style.display = "none";
     elevateClick.style.display = "none";
     intClick.style.display = "none";
     persClick.style.display = "inherit";
 } else {
     persClick.style.display = "none";
+}
+}
+
+function interview1click() {
+    if (int1.style.display = "inherit") {
+        int2.style.display = "none";
+        int3.style.display = "none";
+        int4.style.display = "none";
+    } else {
+        int1.style.display = "none";
+}
+}
+
+function interview2click() {
+    if (int1.style.display = "none") {
+        int2.style.display = "inherit";
+        int3.style.display = "none";
+        int4.style.display = "none";
+    } else {
+        int2.style.display = "none";
+}
+}
+
+function interview3click() {
+    if (int1.style.display = "none") {
+        int2.style.display = "none";
+        int3.style.display = "inherit";
+        int4.style.display = "none";
+    } else {
+        int3.style.display = "none";
+}
+}
+
+function interview4click() {
+    if (int1.style.display = "none") {
+        int2.style.display = "none";
+        int3.style.display = "none";
+        int4.style.display = "inherit";
+    } else {
+        int4.style.display = "none";
 }
 }
